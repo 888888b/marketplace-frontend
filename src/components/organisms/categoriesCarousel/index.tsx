5e2523e6b0f6a21d36517ff91dd8d2f09bfd9991
Vitor Hugo <Hugosilva55442@gmail.com>
2025-05-01 15:59:32 -0300

    style: adjust the size of the category bar icons

--- src/components/organisms/categoriesCarousel/index.tsx
+++ src/components/organisms/categoriesCarousel/index.tsx
@@ -14,91 +14,99 @@ export default function CategoriesCarousel( props: ComponentProps ) {
                 className="gap-x-5" 
                 options={{progressBar: true, dragFree: true}}>
                 <div className="embla__slide">
-                    <Image
-                        src={'/food_icon.png'}
-                        alt="glass and fork icon"
-                        width={35}
-                        height={35}
-                        className="object-cover"
-                    />
+                    <div className="icon-box">
+                        <Image
+                            src={'/food_icon.png'}
+                            alt="glass and fork icon"
+                            fill={true}
+                            className="object-cover"
+                        />
+                    </div>
                     <h4>Alimentação</h4>
                 </div>
 
                 <div className="embla__slide">
-                    <Image
-                        src={'/crane_icon.png'}
-                        alt="glass and fork icon"
-                        width={35}
-                        height={35}
-                        className="object-cover"
-                    />
+                    <div className="icon-box">
+                        <Image
+                            src={'/crane_icon.png'}
+                            alt="glass and fork icon"
+                            fill={true}
+                            className="object-cover"
+                        />
+                    </div>
                     <h4>Construção</h4>
                 </div>
 
                 <div className="embla__slide">
-                    <Image
-                        src={'/bag_icon.png'}
-                        alt="glass and fork icon"
-                        width={36}
-                        height={36}
-                        className="object-cover"
-                    />
+                    <div className="icon-box">
+                        <Image
+                            src={'/bag_icon.png'}
+                            alt="glass and fork icon"
+                            fill={true}
+                            className="object-cover"
+                        />
+                    </div>
                     <h4>Moda</h4>
                 </div>
 
                 <div className="embla__slide">
-                    <Image
-                        src={'/supermarket_icon.png'}
-                        alt="glass and fork icon"
-                        width={35}
-                        height={35}
-                        className="object-cover"
-                    />
+                    <div className="icon-box">
+                        <Image
+                            src={'/supermarket_icon.png'}
+                            alt="glass and fork icon"
+                            fill={true}
+                            className="object-cover"
+                        />
+                    </div>
                     <h4>Mercado</h4>
                 </div>
 
 
                 <div className="embla__slide">
-                    <Image
-                        src={'/laptop_icon.png'}
-                        alt="glass and fork icon"
-                        width={35}
-                        height={35}
-                        className="object-cover"
-                    />
+                    <div className="icon-box">
+                        <Image
+                            src={'/laptop_icon.png'}
+                            alt="glass and fork icon"
+                            fill={true}
+                            className="object-cover"
+                        />
+                    </div>
                     <h4>Eletrônicos</h4>
                 </div>
 
                 <div className="embla__slide">
-                    <Image
-                        src={'/service_icon.png'}
-                        alt="glass and fork icon"
-                        width={35}
-                        height={35}
-                        className="object-cover"
-                    />
+                    <div className="icon-box">
+                        <Image
+                            src={'/service_icon.png'}
+                            alt="glass and fork icon"
+                            fill={true}
+                            className="object-cover"
+                        />
+                    </div>
                     <h4>Serviços</h4>
                 </div>
 
                 <div className="embla__slide">
-                    <Image
-                        src={'/health_icon.png'}
-                        alt="glass and fork icon"
-                        width={36}
-                        height={36}
-                        className="object-cover"
-                    />
+                    <div className="icon-box">
+                        <Image
+                            src={'/health_icon.png'}
+                            alt="glass and fork icon"
+                            fill={true}
+                            className="object-cover"
+                        />
+                    </div>
                     <h4>Saúde & Beleza</h4>
                 </div>
 
                 <div className="embla__slide">
-                    <Image
-                        src={'/pets_icon.png'}
-                        alt="glass and fork icon"
-                        width={35}
-                        height={35}
-                        className="object-cover"
-                    />
+                    <div className="icon-box">
+                        <Image
+                            src={'/pets_icon.png'}
+                            alt="glass and fork icon"
+                            fill={true}
+                            className="object-cover"
+                        />
+                    </div>
                     <h4>Pets</h4>
                 </div>
             </EmblaSlides>
